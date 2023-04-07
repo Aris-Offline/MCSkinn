4574ed3a0acf95768070d5da35585b816ece692a
Yujun <hereisnotyoojun@outlook.com>
2023-04-07 08:18:03 +0800

    Add source code

---
+++ source/MCSkinn/MCSkinnAppContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using MCSkinn.Forms;
+
+namespace MCSkinn
+{
+	class MCSkinnAppContext : ApplicationContext
+	{
+		public Form Form;
+		public Splash SplashForm;
+		public UpdateSystem.UpdateChecker Updater;
+
+		public MCSkinnAppContext()
+		{
+			Program.Context = this;
+			Environment.CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			GlobalSettings.Load();
+
+			try
+			{
+				if (Directory.Exists(GlobalSettings.GetDataURI("__updateFiles")))
+					Directory.Delete(GlobalSettings.GetDataURI("__updateFiles"), true);
+			}
+			catch
+			{
+			}
+
+			SplashForm = new Splash();
+			Updater = new UpdateSystem.UpdateChecker("http://alteredsoftworks.com/mcskinn/update");
+
+			Form = new Editor();
+			Form.FormClosing += (sender, e) => GlobalSettings.Save();
+			Form.FormClosed += (sender, e) => ExitThread();
+
+			SplashForm.Show();
+		}
+
+		public void DoneLoadingSplash()
+		{
+			Form.Show();
+		}
+	}
+}
